d994f2e3df488e78cc28e4a5909711f9c2b1066e
Tito Yslas <roberto.yslas@gmail.com>
2017-12-22 14:12:35 -0700

    code blocks & if/else statements

--- src/com/tito/Main.java
+++ src/com/tito/Main.java
@@ -36,6 +36,42 @@ public class Main {
         int anotherVariable = 50; myVariable --; System.out.println("anutha one!"); //you can many statement on the same line
 
         // indenting helps us to see the logical flow (IntelliJ: ⌥ + ⌘ + L to reformat)
-        
+
+        /*IF STATEMENTS & CODE BLOCKS*/
+        boolean gameOver = true;
+        int newScore = 950;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if (newScore < 5000 && newScore > 1000) {
+//            System.out.println("1000 < score < 5000 "); //can write this if statement w/o curly braces
+//        } else if (newScore < 1000) {
+//            System.out.println("your score < 1000");
+//        }
+//        else {
+//            System.out.println("got here");
+//        }
+
+        if (gameOver == true) {
+            int finalScore = newScore + (levelCompleted * bonus);
+            System.out.println("your final score is: " + finalScore);
+        }
+//        variables created inside of code blocks are not scoped globally, they are scoped to that block
+//        int savedFinalScore = finalScore;
+
+        /* CHALLENGE */
+        // Print a second score to the screen w/the following params
+        // 1. score set to 10_000
+        // 2. levelCompleted set to 8
+        // 3. bonus set to 200
+        // 4. still print line 57
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("your second final score is: " + finalScore);
+        }
     }
 }
