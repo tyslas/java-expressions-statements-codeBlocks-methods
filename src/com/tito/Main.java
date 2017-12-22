1a67273877ab9c220408462a5c9dba1ad83707a4
Tito Yslas <roberto.yslas@gmail.com>
2017-12-22 15:55:20 -0700

    intro to methods

--- src/com/tito/Main.java
+++ src/com/tito/Main.java
@@ -3,7 +3,7 @@ package com.tito;
 public class Main {
 
     public static void main(String[] args) {
-    // KEYWORDS & EXPRESSIONS
+    /* KEYWORDS & EXPRESSIONS */
         // ml = 1.609344km
         double km = (100 * 1.609334);
         int highScore = 50;
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("this is an expression");
         }
 
-        //CHALLENGE:
+    /* CHALLENGE: */
         //in the code below identify which parts are expressions
         //semi-colons and data types are not included
 
@@ -23,7 +23,7 @@ public class Main {
             score = 0; // line 24  expression: score = 0
         }
 
-    // STATEMENTS & WHITESPACE
+    /* STATEMENTS & WHITESPACE */
         int myVariable = 50; //lines 27-30 are statements
         myVariable ++;
         myVariable --;
@@ -37,41 +37,50 @@ public class Main {
 
         // indenting helps us to see the logical flow (IntelliJ: ⌥ + ⌘ + L to reformat)
 
-        /*IF STATEMENTS & CODE BLOCKS*/
+    /*IF STATEMENTS & CODE BLOCKS*/
         boolean gameOver = true;
-        int newScore = 950;
+        int newScore = 800;
         int levelCompleted = 5;
         int bonus = 100;
+        calculateScore(gameOver, newScore, levelCompleted, bonus); //uses method from line 65
 
-//        if (newScore < 5000 && newScore > 1000) {
-//            System.out.println("1000 < score < 5000 "); //can write this if statement w/o curly braces
-//        } else if (newScore < 1000) {
-//            System.out.println("your score < 1000");
-//        }
-//        else {
-//            System.out.println("got here");
-//        }
+        gameOver = false;
+        newScore = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        System.out.println(calculateScore(gameOver, newScore, levelCompleted, bonus));
 
-        if (gameOver == true) {
-            int finalScore = newScore + (levelCompleted * bonus);
-            System.out.println("your final score is: " + finalScore);
-        }
 //        variables created inside of code blocks are not scoped globally, they are scoped to that block
 //        int savedFinalScore = finalScore;
 
-        /* CHALLENGE */
+    /* CHALLENGE */
         // Print a second score to the screen w/the following params
         // 1. score set to 10_000
         // 2. levelCompleted set to 8
         // 3. bonus set to 200
-        // 4. still print line 57
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        // 4. still print line 57 **edited to use a method
+//        boolean gameEnded = true;
+//        int newScore = 10000;
+//        int level= 8;
+//        int newBonus = 200;
+//
+//        if (gameEnded) {
+//            int finalScore = newScore + (level * newBonus);
+//            System.out.println("[CHALLENGE] your second final score is: " + finalScore);
+//        }
+    }
+    /* METHODS */
+    // calculateScore method below
+    // void keyword -> don't send any value back
 
-        if (gameOver == true) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("your second final score is: " + finalScore);
+            System.out.println("[calculateScore method] your final score is: " + finalScore);
+            return finalScore;
         }
+        return -1;
+        
     }
 }
